0b4c18a52a98ce0a6841df62c8d572a678481755
Luis Martinez <lmartinez@fidenslat.com>
2024-09-02 16:52:24 -0500

    Comit Nombre

--- frontend/src/app/models/socio.model.ts
+++ frontend/src/app/models/socio.model.ts
@@ -1,7 +1,7 @@
 export interface Socio {
     id: number;
-    nombre1: string;
-    apellido1: string;
+    nombre: string;
+    apellido: string;
     descripcion: string;
     inversionTotal: number;
     creditoTotal: number;

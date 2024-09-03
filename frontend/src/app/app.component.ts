77f7ad6c230638bbb30f12fc4a3cd313a626cdbd
Luis Martinez <lmartinez@fidenslat.com>
2024-09-03 17:08:49 -0500

    CDS

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,7 +4,3 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'poo-servicio02-angular';
-}
